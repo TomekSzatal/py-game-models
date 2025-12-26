6a8030f2d60f56d6b284ceb879e7bd79c1bea2ae
Tomasz Szatałowicz <szatalowicztomek@gmail.com>
2025-12-26 11:22:18 +0100

    v2

--- main.py
+++ main.py
@@ -6,36 +6,36 @@ from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    with open('players.json', 'r') as file:
+    with open("players.json", "r") as file:
         players = json.load(file)
 
     for player in players:
-        race_info = player.get('race', {})
+        race_info = player.get("race", {})
         race, _ = Race.objects.get_or_create(
-            name=race_info.get('name'),
-            defaults={'description': race_info.get('description', '')}
+            name=race_info.get("name"),
+            defaults={"description": race_info.get("description", "")}
         )
-        guild_info = player.get('guild', {})
-        guild_name = guild_info.get('name')
+        guild_info = player.get("guild", {})
+        guild_name = guild_info.get("name")
         guild = None
         if guild_name:
             guild, _ = Guild.objects.get_or_create(
                 name=guild_name,
-                defaults={'description': guild_info.get('description', '')}
+                defaults={"description": guild_info.get("description", "")}
             )
-        for skill_data in player.get('skills', []):
+        for skill_data in player.get("skills", []):
             Skill.objects.get_or_create(
-                name=skill_data.get('name'),
+                name=skill_data.get("name"),
                 race=race,
-                defaults={'bonus': skill_data.get('bonus', '')}
+                defaults={"bonus": skill_data.get("bonus", "")}
             )
         Player.objects.get_or_create(
-            nickname=player['nickname'],
+            nickname=player["nickname"],
             defaults={
-                'email': player.get('email', ''),
-                'bio': player.get('bio', ''),
-                'race': race,
-                'guild': guild
+                "email": player.get("email", ""),
+                "bio": player.get("bio", ""),
+                "race": race,
+                "guild": guild
             }
         )
 
